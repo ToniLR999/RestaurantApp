a726f8e3aab5ca65aef41ca8f68fea2dfe562a85
ToniLR999 <tonilupianezroman@gmail.com>
2024-09-25 23:51:51 +0200

    Packages

---
+++ RestaurantAppBackEndSpringBoot/src/main/java/com/tonilr/RestaurantApp/Configurations/AppConfig.java
@@ -0,0 +1,14 @@
+package com.tonilr.RestaurantApp.Configurations;
+
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class AppConfig {
+
+    //@Bean
+    //public RestTemplate restTemplate() {
+     //   return new RestTemplate();
+    //}
+	
+}
